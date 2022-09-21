05e8fbf5bd2327b612266ced067cda27a40d4cc6
Cristian de Mellos <cristian.ms@hotmail.com>
2022-09-20 22:58:49 -0300

    Limpeza de código desnecessário no novo modelo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 // ignore_for_file: avoid_print, import_of_legacy_library_into_null_safe, depend_on_referenced_packages
 
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:cross_file/cross_file.dart';
-import 'package:desktop_drop/desktop_drop.dart';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:excel/excel.dart';
 import 'package:flutter/foundation.dart';
@@ -55,47 +53,21 @@ class _MyHomePageState extends State<MyHomePage> {
           appBar: AppBar(
             title: const Text('XLS Test'),
           ),
-          body: DropTarget(
-            onDragDone: (detail) {
-              setState(() {
-                listaArquivosLeitura.addAll(detail.files);
-                final first = detail.files.first;
-                caminhoDestino = first.path.replaceAll('\\${first.name}', '').replaceAll('\\', '/');
-                print(' > caminhoDestino: $caminhoDestino');
-              });
-            },
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text('Arraste o arquivo gerado no aplicativo "Ponto Fácil" para cá!'),
-                  // Ícone de pasta
-                  Text(
-                    parser.get('open_file_folder').code,
-                    style: const TextStyle(fontSize: 50),
-                  ),
-                  // Lista de arquivos em memória
-                  if (listaArquivosLeitura.isNotEmpty) ...[
-                    const Divider(),
-                    ...listaArquivosLeitura.map((e) => Text(e.path)).toList(),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16, bottom: 16),
-                      child: GestureDetector(
-                        child: const Text('Limpar arquivos', style: TextStyle(fontWeight: FontWeight.bold)),
-                        onTap: () {
-                          setState(() {
-                            listaArquivosLeitura.clear();
-                          });
-                        },
-                      ),
-                    ),
-                  ]
-                ],
-              ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Clique para fazer a leitura do arquivo de backup no seu Google Drive'),
+                // Ícone de pasta
+                Text(
+                  parser.get('open_file_folder').code,
+                  style: const TextStyle(fontSize: 50),
+                ),
+              ],
             ),
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: _geraXls,
+            onPressed: () {},
             tooltip: 'Gera arquivo',
             child: const Icon(Icons.file_copy),
           ),
@@ -104,126 +76,126 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  String _letraCorrespondente(int n) {
-    switch (n) {
-      case 0:
-        return 'A';
-      case 1:
-        return 'B';
-      case 2:
-        return 'C';
-      case 3:
-        return 'D';
-      case 4:
-        return 'E';
-      case 5:
-        return 'F';
-      case 6:
-        return 'G';
-      case 7:
-        return 'H';
-      case 8:
-        return 'I';
-      case 9:
-        return 'J';
-      case 10:
-        return 'K';
-      case 11:
-        return 'L';
-      case 12:
-        return 'M';
-      case 13:
-        return 'N';
-      case 14:
-        return 'O';
-      case 15:
-        return 'P';
-      case 16:
-        return 'Q';
-      case 17:
-        return 'R';
-      case 18:
-        return 'S';
-      case 19:
-        return 'T';
-      case 20:
-        return 'U';
-      case 21:
-        return 'V';
-      case 22:
-        return 'X';
-      case 23:
-        return 'Z';
-      case 24:
-        return 'W';
-      case 25:
-        return 'Y';
-      case 26:
-        return 'AA';
-      case 27:
-        return 'AB';
-      case 28:
-        return 'AC';
-      case 29:
-        return 'AD';
-      case 30:
-        return 'AE';
-    }
-    return '';
-  }
+  // String _letraCorrespondente(int n) {
+  //   switch (n) {
+  //     case 0:
+  //       return 'A';
+  //     case 1:
+  //       return 'B';
+  //     case 2:
+  //       return 'C';
+  //     case 3:
+  //       return 'D';
+  //     case 4:
+  //       return 'E';
+  //     case 5:
+  //       return 'F';
+  //     case 6:
+  //       return 'G';
+  //     case 7:
+  //       return 'H';
+  //     case 8:
+  //       return 'I';
+  //     case 9:
+  //       return 'J';
+  //     case 10:
+  //       return 'K';
+  //     case 11:
+  //       return 'L';
+  //     case 12:
+  //       return 'M';
+  //     case 13:
+  //       return 'N';
+  //     case 14:
+  //       return 'O';
+  //     case 15:
+  //       return 'P';
+  //     case 16:
+  //       return 'Q';
+  //     case 17:
+  //       return 'R';
+  //     case 18:
+  //       return 'S';
+  //     case 19:
+  //       return 'T';
+  //     case 20:
+  //       return 'U';
+  //     case 21:
+  //       return 'V';
+  //     case 22:
+  //       return 'X';
+  //     case 23:
+  //       return 'Z';
+  //     case 24:
+  //       return 'W';
+  //     case 25:
+  //       return 'Y';
+  //     case 26:
+  //       return 'AA';
+  //     case 27:
+  //       return 'AB';
+  //     case 28:
+  //       return 'AC';
+  //     case 29:
+  //       return 'AD';
+  //     case 30:
+  //       return 'AE';
+  //   }
+  //   return '';
+  // }
 
-  void _geraXls() {
-    if (listaArquivosLeitura.isNotEmpty) {
-      for (var i = 0; i < listaArquivosLeitura.length; i++) {
-        final caminhoArquivoLeitura = listaArquivosLeitura[i].path;
-        final bytesArquivoLeitura = File(caminhoArquivoLeitura).readAsBytesSync();
-        // Inicializa obj Excel para leitura
-        final excelLeitura = Excel.decodeBytes(bytesArquivoLeitura);
-        // Inicializa obj Excel para escrita
-        final excelCriacao = Excel.createExcel();
-        final aba = excelCriacao['Sheet1'];
-        // Percorre cada aba do Excel
-        for (var abaLeituraAtual in excelLeitura.tables.keys) {
-          var identificadorDataAtual = '';
-          // Percorre cada aba do Excel
-          for (var k = 0; k < excelLeitura.tables[abaLeituraAtual]!.rows.length; k++) {
-            if (k >= 8) {
-              final linha = excelLeitura.tables[abaLeituraAtual]!.rows[k];
-              if (linha[1] != null &&
-                  linha[1].toString().contains('Data(') &&
-                  !linha[0].toString().contains('Nenhu') &&
-                  !linha[1].toString().contains('Nenhu') &&
-                  !linha[2].toString().contains('Nenhu')) {
-                identificadorDataAtual = linha[1].toString();
-                final registroDiario = RegistroDiario(identificador: identificadorDataAtual);
-                registroDiario.addHorario(linha[2].toString());
-                listaRegistrosDiarios.add(registroDiario);
-              } else if (linha[1] == null && linha[2].toString().contains('Data(') && !linha[2].toString().contains('Resumo')) {
-                listaRegistrosDiarios.firstWhere((element) => element.identificador == identificadorDataAtual).addHorario(linha[2].toString());
-              }
-            }
-          }
-        }
+  // void _geraXls() {
+  //   if (listaArquivosLeitura.isNotEmpty) {
+  //     for (var i = 0; i < listaArquivosLeitura.length; i++) {
+  //       final caminhoArquivoLeitura = listaArquivosLeitura[i].path;
+  //       final bytesArquivoLeitura = File(caminhoArquivoLeitura).readAsBytesSync();
+  //       // Inicializa obj Excel para leitura
+  //       final excelLeitura = Excel.decodeBytes(bytesArquivoLeitura);
+  //       // Inicializa obj Excel para escrita
+  //       final excelCriacao = Excel.createExcel();
+  //       final aba = excelCriacao['Sheet1'];
+  //       // Percorre cada aba do Excel
+  //       for (var abaLeituraAtual in excelLeitura.tables.keys) {
+  //         var identificadorDataAtual = '';
+  //         // Percorre cada aba do Excel
+  //         for (var k = 0; k < excelLeitura.tables[abaLeituraAtual]!.rows.length; k++) {
+  //           if (k >= 8) {
+  //             final linha = excelLeitura.tables[abaLeituraAtual]!.rows[k];
+  //             if (linha[1] != null &&
+  //                 linha[1].toString().contains('Data(') &&
+  //                 !linha[0].toString().contains('Nenhu') &&
+  //                 !linha[1].toString().contains('Nenhu') &&
+  //                 !linha[2].toString().contains('Nenhu')) {
+  //               identificadorDataAtual = linha[1].toString();
+  //               final registroDiario = RegistroDiario(identificador: identificadorDataAtual);
+  //               registroDiario.addHorario(linha[2].toString());
+  //               listaRegistrosDiarios.add(registroDiario);
+  //             } else if (linha[1] == null && linha[2].toString().contains('Data(') && !linha[2].toString().contains('Resumo')) {
+  //               listaRegistrosDiarios.firstWhere((element) => element.identificador == identificadorDataAtual).addHorario(linha[2].toString());
+  //             }
+  //           }
+  //         }
+  //       }
 
-        var indexLinhaDia = 1; // Um dia por linha
-        for (final dia in listaRegistrosDiarios) {
-          final horariosPorDia = dia.horarios;
-          aba.cell(CellIndex.indexByString('A$indexLinhaDia')).value = dia.identificador;
-          for (var indexHorarioColuna = 0; indexHorarioColuna < horariosPorDia.length; indexHorarioColuna++) {
-            final letra = _letraCorrespondente(indexHorarioColuna + 1);
-            aba.cell(CellIndex.indexByString('$letra$indexLinhaDia')).value = horariosPorDia[indexHorarioColuna];
-          }
-          indexLinhaDia++;
-        }
+  //       var indexLinhaDia = 1; // Um dia por linha
+  //       for (final dia in listaRegistrosDiarios) {
+  //         final horariosPorDia = dia.horarios;
+  //         aba.cell(CellIndex.indexByString('A$indexLinhaDia')).value = dia.identificador;
+  //         for (var indexHorarioColuna = 0; indexHorarioColuna < horariosPorDia.length; indexHorarioColuna++) {
+  //           final letra = _letraCorrespondente(indexHorarioColuna + 1);
+  //           aba.cell(CellIndex.indexByString('$letra$indexLinhaDia')).value = horariosPorDia[indexHorarioColuna];
+  //         }
+  //         indexLinhaDia++;
+  //       }
 
-        final caminhoArquivoEscrita = '$caminhoDestino/form_${i + 1}.xlsx';
-        final bytesArquivoEscrita = excelCriacao.encode();
-        File(join(caminhoArquivoEscrita))
-          ..createSync(recursive: true)
-          ..writeAsBytesSync(bytesArquivoEscrita!);
-      }
-    }
-  }
+  //       final caminhoArquivoEscrita = '$caminhoDestino/form_${i + 1}.xlsx';
+  //       final bytesArquivoEscrita = excelCriacao.encode();
+  //       File(join(caminhoArquivoEscrita))
+  //         ..createSync(recursive: true)
+  //         ..writeAsBytesSync(bytesArquivoEscrita!);
+  //     }
+  //   }
+  // }
 
 // /// Data(07:56 - 11:28, 2, 10, null, Extrato)
 //   String _trataHorario(String strXls) {
